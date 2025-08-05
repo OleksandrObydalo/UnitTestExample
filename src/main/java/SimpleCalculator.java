606948dd3a4d118bcde87e04392375637735c0b8
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-08-05 12:34:56 +0300

    Add one more test method, and make program pass only one test

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,5 +1,5 @@
 public class SimpleCalculator {
     public int add(int a, int b){
-        return a + b;
+        return a * b;
     }
 }
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -6,6 +6,12 @@ class SimpleCalculatorTest {
     @Test
     void twoPlusTwoShouldEqualFour(){
         var calculator = new SimpleCalculator();
-        assertEquals(4, calculator.add(2,2));
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    void threePlusSevenShouldEqualTen(){
+        var calculator = new SimpleCalculator();
+        assertEquals(10, calculator.add(3, 7));
     }
 }
